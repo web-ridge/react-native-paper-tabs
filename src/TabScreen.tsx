0dd400bd80e8d55d9e622c2e3587137a31f03323
Cyril <cyril.moreauu@gmail.com>
2024-03-13 13:44:05 +0000

    update badgeStyle to badgeProps

--- src/TabScreen.tsx
+++ src/TabScreen.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
-import type { GestureResponderEvent } from 'react-native';
+import type { Animated, GestureResponderEvent } from 'react-native';
 import type { IconSource } from 'react-native-paper/lib/typescript/src/components/Icon';
+import type { ThemeProps } from 'react-native-paper/lib/typescript/src/types';
+import { StyleProp, TextStyle } from 'react-native';
+export interface BadgeProps {
+  visible?: boolean;
+  size?: number;
+  style?: StyleProp<TextStyle>;
+  ref?: React.RefObject<typeof Animated.Text>;
+  theme?:ThemeProps;
+}
 
 export interface TabScreenProps {
   label: string;
   icon?: IconSource;
   badge?: string | number | boolean;
-  badgeStyle?: React.CSSProperties;
+  badgeProps?: BadgeProps;
   children: any;
   onPress?: (event: GestureResponderEvent) => void;
   onPressIn?: (event: GestureResponderEvent) => void;
